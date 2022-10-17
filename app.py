694988b421f4c0b15210c6a8af23d76fd2e78548
Charlie Xiang <yx2017@nyu.edu>
2022-10-17 14:52:26 -0400

    added create routes for both user and moderator

--- app.py
+++ app.py
@@ -3,62 +3,173 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 from markupsafe import escape
 import pymongo
 import datetime
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from dotenv import dotenv_values
+
+import pymongo
+import datetime
 from bson.objectid import ObjectId
-import os
-import subprocess
+import sys
 
 # instantiate the app
 app = Flask(__name__)
 
 # load credentials and configuration options from .env file
 # if you do not yet have a file named .env, make one based on the template in env.example
-import credentials
-config = credentials.get()
+config = dotenv_values(".env")
 
 # turn on debugging if in development mode
 if config['FLASK_ENV'] == 'development':
     # turn on debugging, if in development
     app.debug = True # debug mnode
 
-# make one persistent connection to the database
-connection = pymongo.MongoClient(config['MONGO_HOST'], 27017, 
-                                username=config['MONGO_USER'],
-                                password=config['MONGO_PASSWORD'],
-                                authSource=config['MONGO_DBNAME'])
-db = connection[config['MONGO_DBNAME']] # store a reference to the database
 
+# connect to the database
+cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
+try:
+    # verify the connection works by pinging the database
+    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
+    db = cxn[config['MONGO_DBNAME']] # store a reference to the database
+    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    # render_template('error.html', error=e) # render the edit template
+    print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
+    print('Database connection error:', e) # debug
 
-# routes
+# set up the routes
 
-@app.route('/')
+# route for the user home page
+@app.route('/user')
 def home():
     """
     Route for the home page
     """
-    return render_template('home.html')
+    docs = db.exampleapp.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
+    return render_template('home.html', docs=docs) # render the hone template
 
-
-###
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
+# route for the moderator home page
+@app.route('/moderator')
+def home():
     """
-    GitHub can be configured such that each time a push is made to a repository, GitHub will make a request to a particular web URL... this is called a webhook.
-    This function is set up such that if the /webhook route is requested, Python will execute a git pull command from the command line to update this app's codebase.
-    You will need to configure your own repository to have a webhook that requests this route in GitHub's settings.
-    Note that this webhook does do any verification that the request is coming from GitHub... this should be added in a production environment.
+    Route for the home page
     """
-    # run a git pull command
-    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    pull_output = process.communicate()[0]
-    # pull_output = str(pull_output).strip() # remove whitespace
-    process = subprocess.Popen(["chmod", "a+x", "flask.cgi"], stdout=subprocess.PIPE)
-    chmod_output = process.communicate()[0]
-    # send a success response
-    response = make_response('output: {}'.format(pull_output), 200)
-    response.mimetype = "text/plain"
-    return response
+    docs = db.exampleapp.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
+    return render_template('moderator_home.html', docs=docs) # render the hone template
 
+# route to accept form submission and create a new post
+@app.route('/user/add', methods=['POST'])
+def add_spot_u():
+    """
+    Route for POST requests to the create page.
+    Accepts the form submission data for a new document and saves the document to the database.
+    """
+    spot_name = request.form['fspotname']
+    address = request.form['faddress']
+    location = request.form['flocation']
+    type = request.form['ftype']
+    purchase = request.form['fpurchase']
+    noise = request.form['fnoise']
+    description = request.form['fdescription']
+
+
+    # create a new document with the data the user entered
+    doc = {
+        "spot_name": spot_name,
+        "message": address, 
+        "location": location,
+        "type": type,
+        "purchase": purchase,
+        "noise": noise,
+        "description": description,
+        "created_at": datetime.datetime.utcnow()
+    }
+    db.exampleapp.insert_one(doc) # insert a new document
+
+    return redirect(url_for('home')) # tell the browser to make a request for the / route (the home function)
+
+
+# route to accept form submission and create a new post
+@app.route('/moderator/add', methods=['POST'])
+def add_spot_m():
+    """
+    Route for POST requests to the create page.
+    Accepts the form submission data for a new document and saves the document to the database.
+    """
+    spot_name = request.form['fspotname']
+    address = request.form['faddress']
+    location = request.form['flocation']
+    type = request.form['ftype']
+    purchase = request.form['fpurchase']
+    noise = request.form['fnoise']
+    description = request.form['fdescription']
+
+
+    # create a new document with the data the user entered
+    doc = {
+        "spot_name": spot_name,
+        "message": address, 
+        "location": location,
+        "type": type,
+        "purchase": purchase,
+        "noise": noise,
+        "description": description,
+        "created_at": datetime.datetime.utcnow()
+    }
+    db.exampleapp.insert_one(doc) # insert a new document
+
+    return redirect(url_for('moderator_home')) # tell the browser to make a request for the / route (the home function)
+
+
+# route to view the edit form for an existing post
+@app.route('/edit/<mongoid>')
+def edit(mongoid):
+    """
+    Route for GET requests to the edit page.
+    Displays a form users can fill out to edit an existing record.
+    """
+    doc = db.exampleapp.find_one({"_id": ObjectId(mongoid)})
+    return render_template('edit.html', mongoid=mongoid, doc=doc) # render the edit template
+
+
+# route to accept the form submission to delete an existing post
+@app.route('/edit/<mongoid>', methods=['POST'])
+def edit_post(mongoid):
+    """
+    Route for POST requests to the edit page.
+    Accepts the form submission data for the specified document and updates the document in the database.
+    """
+    name = request.form['fname']
+    message = request.form['fmessage']
+
+    doc = {
+        # "_id": ObjectId(mongoid), 
+        "name": name, 
+        "message": message, 
+        "created_at": datetime.datetime.utcnow()
+    }
+
+    db.exampleapp.update_one(
+        {"_id": ObjectId(mongoid)}, # match criteria
+        { "$set": doc }
+    )
+
+    return redirect(url_for('home')) # tell the browser to make a request for the / route (the home function)
+
+# route to delete a specific post
+@app.route('/delete/<mongoid>')
+def delete(mongoid):
+    """
+    Route for GET requests to the delete page.
+    Deletes the specified record from the database, and then redirects the browser to the home page.
+    """
+    db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
+    return redirect(url_for('home')) # tell the web browser to make a request for the / route (the home function)
+
+
+# route to handle any errors
 @app.errorhandler(Exception)
 def handle_error(e):
     """
@@ -67,6 +178,7 @@ def handle_error(e):
     return render_template('error.html', error=e) # render the edit template
 
 
+# run the app
 if __name__ == "__main__":
     #import logging
     #logging.basicConfig(filename='/home/ak8257/error.log',level=logging.DEBUG)
